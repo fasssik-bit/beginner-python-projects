48cbdefdf34a517b6094ea20d6cbff5d20d97cb9
fasssik-bit <fasihmuhammad218@gmail.com>
2026-04-15 21:47:37 +0500

    Add files via upload

---
+++ assigment3.py
@@ -0,0 +1,7 @@
+#madlip excersice
+place = input("Enter a place : ")
+animal = input("Enter the animal you saw: ")
+Name = input("enter the person name: ")
+action = input("enter the action: ")
+story=f"today i went to the {place}.I saw an {animal}.then {Name} came asn started a {action}"
+print(story)
